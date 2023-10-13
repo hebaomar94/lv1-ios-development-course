cb164a538497d2f6b5466c63da0cc4d55ab30a40
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-10-13 08:31:14 +0300

    Update exercise2.swift

--- exercise2.swift
+++ exercise2.swift
@@ -5,3 +5,72 @@ func whatTypeIsiT(input:String) -> Bool {
     input.hasSuffix("s")    
 }
 print (whatTypeIsiT(input: "apples"))
+
+//2
+/*Convert Age to Days
+Write a function that takes the age in years and returns the age in days. For example calculateAgeDays(20) -> 7300
+*/
+
+func calculateAgeDays (age: Int) -> Int {
+  let daysInYear = 365
+    return age * daysInYear
+}
+print (calculateAgeDays(age: 26))
+
+
+//4. Reverse a string
+//reversed()
+//Write a function that takes an input string and reverses its order. For example, “Hello!” should return “!olleh”.
+func reverse (x: String) -> String {
+ return String(x.reversed())
+}
+print(reverse(x: "Hello"))
+
+//5 
+/* Is there a higher number?
+Write a function that takes an array and number, n, and returns TRUE if a number in the array is greater than n. For example, for an array [24, 32, 42, 34] and n = 50, the output should be FALSE.
+*/
+
+func  isthereahighernumber (x: [Int] , n: Int ) -> Bool {
+    for element in x {
+         if n > element {
+             return true
+         }
+     }
+     return false
+ }
+
+
+let new = isthereahighernumber(x:  [24, 32, 42, 34], n: 50)
+print (new)
+
+/*6. Basic calculator
+Write a class that contains functions to do summation, submission, multiply, and divide for 2 numbers.
+*/
+class Calc {
+    var x: Int
+    var y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func sum (x: Int , y: Int )-> Int{
+        return x+y
+    }
+    func sub (x: Int , y: Int )->Int{
+        return x-y
+    }
+    func div (x: Int , y: Int )->Int{
+        return x/y
+    }
+}
+let basicCalc = Calc(x: 4 , y: 8)
+
+print(basicCalc.sum(x: 4 , y: 8))
+print(basicCalc.sub(x: 4 , y: 8))
+print(basicCalc.div(x: 4 , y: 8))
+
+
+
